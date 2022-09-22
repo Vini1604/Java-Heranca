cd13fb9f9ddb90cd8c5f551644ff5b81d76cbf94
Vini1604 <vinicius.oliveira1998@hotmail.com>
2022-09-21 23:48:23 -0300

    Corrigido a versao do Java

--- src/main/java/uscs/TesteAluno.java
+++ src/main/java/uscs/TesteAluno.java
@@ -5,7 +5,7 @@ public class TesteAluno {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa("Jose", "Rua Antonio Marques, 30", "568.234.981-15", "3.698.222-8");
         Aluno a1 = new Aluno("Pedro", "Rua Almeida, 20", "456.876.123-12", "4.687.098-2", 59751, 4.5, 8.0, 10);
-        Aluno a2 = new Aluno("Francisco", "Rua Almeida, 34", "123.465.891-12", "4.687.456-2", 59751, 4.5, 1.0, 5);
+        Aluno a2 = new Aluno("Francisco", "Rua dos Abacates, 34", "123.465.891-12", "5.157.456-2", 80971, 8.0, 9.0, 11);
         p1.ImprimePessoa();
         System.out.println("");
         a1.ImprimeAluno();
